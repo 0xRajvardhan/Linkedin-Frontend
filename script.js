8e1d047c0870062ccb8232a10ba59225460bd66e
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-29 22:30:25 +0530

    added responsiveness and javascript for activity section toggle

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ let profileMenu = document.getElementById("profileMenu");
 function toggleMenu() {
     profileMenu.classList.toggle("open-menu");
 }
+
+// show more toggle
+
+let sidebarActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity() {
+    sidebarActivity.classList.toggle("open-activity");
+
+    if(sidebarActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show Less <b>-</b>";
+    }
+    else{
+        moreLink.innerHTML = "Show more <b>+</b>"
+    }
+}
